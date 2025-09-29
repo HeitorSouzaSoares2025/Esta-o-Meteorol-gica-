fb547493b0d6f4d6e6035447c19aba7dd20e1325
HeitorSouza_Soares <heitorinformaticasala09@gmail.com>
2025-09-28 23:08:21 -0300

    Update secrets.h.ino

--- arduino/DHT22/secrets.h.ino
+++ arduino/DHT22/secrets.h.ino
@@ -1,4 +1,4 @@
 // secrets.h
-#define WIFI_SSID     "HEITOR_5G"          // Nome da rede Wi-Fi
-#define WIFI_PASSWORD "0107081970Nr"      // Senha da rede
-#define TS_WRITE_KEY  "R7JASIKSUV7QEOZ8"  // Chave Write do ThingSpeak
+#define WIFI_SSID     ""          // Nome da rede Wi-Fi
+#define WIFI_PASSWORD ""      // Senha da rede
+#define TS_WRITE_KEY  ""  // Chave Write do ThingSpeak
